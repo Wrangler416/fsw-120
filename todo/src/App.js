462ec7e1b65fa7ea1c538e00546803404404c0a8
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-24 18:53:32 -0700

    Week1 ToDo

--- todo/src/App.js
+++ todo/src/App.js
@@ -4,6 +4,7 @@
 function App() {
   return (
     <div>
+      <h1>ToDo List</h1>
       <p><input type="checkbox" name="grocery-shopping" />Grocery Shopping</p>
       <p><input type="checkbox" name="yard-work" />Yard Work</p>
       <p><input type="checkbox" name="wash-cars" />Wash Cars</p>
