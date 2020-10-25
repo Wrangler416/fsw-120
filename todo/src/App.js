b0e3af47ef1e450b7922b960aeac9c03befd4f4e
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-24 18:45:22 -0700

    Week1 ToDo

---
+++ todo/src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p><input type="checkbox" name="grocery-shopping" />Grocery Shopping</p>
+      <p><input type="checkbox" name="yard-work" />Yard Work</p>
+      <p><input type="checkbox" name="wash-cars" />Wash Cars</p>
+    </div>
+    /*
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    */
+  );
+}
+
+export default App;
