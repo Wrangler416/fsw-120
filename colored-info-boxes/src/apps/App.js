93207966e3186baf0b3f3b70ba883ebd39fb59a7
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-27 10:26:33 -0700

    Colored Info Boxes

---
+++ colored-info-boxes/src/apps/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import InfoBox from "../components/InfoBox"
+
+function App() {
+    return (
+        <div>
+            <InfoBox />
+        </div>
+    )
+}
+
+export default App
---
+++ colored-info-boxes/src/components/InfoBox.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function InfoBox(props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default InfoBox
