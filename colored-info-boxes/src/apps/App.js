42e3ac59f21e00ffe3f1d487e0640255e9c188e8
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-10-27 17:00:06 -0700

    Colored Info Boxes

--- colored-info-boxes/src/apps/App.js
+++ colored-info-boxes/src/apps/App.js
@@ -3,22 +3,32 @@ import InfoBox from "../components/InfoBox"
 
 function App() {
     return (
-        <div>
+        <div style={{display: "flex"}}>
             <InfoBox 
-                backgroundColor="steelblue"
+                backgroundColor="orange"
                 title="Box 1"
                 subTitle="Sub-Title 1"
                 info="Information for box 1" />
             <InfoBox 
-                backgroundColor="salmon"
+                backgroundColor="brown"
                 title="Box 2"
                 subTitle="Sub-Title 2"
                 info="Information for box 2" />
             <InfoBox 
-                backgroundColor="cyan"
+                backgroundColor="red"
                 title="Box 3"
                 subTitle="Sub-Title 3"
                 info="Information for box 3" />
+            <InfoBox 
+                backgroundColor="yellow"
+                title="Box 4"
+                subTitle="Sub-Title 4"
+                info="Information for box 4" />
+            <InfoBox 
+                backgroundColor="orange"
+                title="Box 5"
+                subTitle="Sub-Title 5"
+                info="Information for box 5" />
         </div>
     )
 }
