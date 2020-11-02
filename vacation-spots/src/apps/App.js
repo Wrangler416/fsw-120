4dbe5512f3b31a3f0895cfd733994b23d0af4fa9
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-11-01 19:56:01 -0800

    Vacation Spots

--- vacation-spots/src/apps/App.js
+++ vacation-spots/src/apps/App.js
@@ -3,9 +3,31 @@ import vacationSpots from "../vacationSpots";
 import Card from "../components/Card";
 
 function App() {
-    const cardComponents = vacationSpots.map(spot => <Card />)
+
+    const cardComponents = vacationSpots.map(function(spot) {
+        let bgColor;
+        let dollarSigns;
+
+        spot.timeToGo === 'Spring' ? bgColor = 'pink'
+            : spot.timeToGo === 'Summer' ? bgColor = 'yellow'
+            : spot.timeToGo === 'Fall' ? bgColor = 'orange'
+            : bgColor = 'lightblue';
+
+        spot.price < 500 ? dollarSigns = '$' 
+            : spot.price < 1000 ? dollarSigns = '$$' 
+            : dollarSigns = '$$$';
+
+        return <Card 
+            key={spot.id}
+            place={spot.place} 
+            price={spot.price} 
+            timeToGo={spot.timeToGo}
+            bgColor={bgColor}
+            dollarSigns={dollarSigns} />
+    })
+
     return (
-        <div>
+        <div style={{display: "flex"}}>
             {cardComponents}
         </div>
     )
--- vacation-spots/src/components/Card.js
+++ vacation-spots/src/components/Card.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function Card(props) {
+    const styles = {
+        fontFamily: "Arial, Helvetica, sans-serif", 
+        textAlign: "center", 
+        width: "200px", 
+        height: "120px", 
+        border: "1px solid black", 
+        padding: "5px", 
+        borderRadius: "10px", 
+        backgroundColor: props.bgColor
+    }
     return (
-        <div>
-
+        <div style={styles}>
+            <h3>{props.place} {props.dollarSigns}</h3>
+            <div>Cost: ${props.price}</div>
+            <div>Best time to go: {props.timeToGo}</div>
         </div>
     )
 }
