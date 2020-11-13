c601dab2a6c727a04f9f0c6fc56409d10fba51d5
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-11-12 22:37:53 -0800

    React Name Badge

--- react-name-badge/src/apps/App.js
+++ react-name-badge/src/apps/App.js
@@ -6,19 +6,31 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            value: '',
-            firstName: ''
+            firstName: '',
+            lastName: ''
         }
-        this.handleClick = this.handleClick.bind(this)
+        this.handleChange = this.handleChange.bind(this)
         this.handleClick = this.handleClick.bind(this)
     }
 
     handleChange(event) {
-
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
     }
 
     handleClick(event) {
 
+    }
+
+    completed() {
+        if (this.state.firstName 
+            && this.state.lastName) {
+            return true
+        } else {
+            return false
+        }
     }
 
     render() {
@@ -26,16 +38,20 @@ class App extends React.Component {
         return(
             <div>
                 <form>
-
                     <input 
                         type="text" 
                         value={this.state.firstName} 
                         name="firstName" 
-                        placeholder="First Name"
+                        placeholder="First Name" 
                         onChange={this.handleChange} />
-
+                    <input 
+                        type="text" 
+                        value={this.state.lastName} 
+                        name="lastName" 
+                        placeholder="Last Name" 
+                        onChange={this.handleChange} />
                     <button 
-                        disabled={!this.state.value}
+                        disabled={!this.completed()} 
                         onClick={this.handleClick}>Submit</button>
                 </form>
             </div>
