db7bfe10df09e00556b8327a14e9f4b3f3fe1df5
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-11-16 13:09:44 -0800

    React Name Badge

--- react-name-badge/src/components/Badges.js
+++ react-name-badge/src/components/Badges.js
@@ -2,7 +2,7 @@ import React from "react"
 import Badge from "../components/Badge"
 
 function Badges(props) {
-    const badges = props.badges.reverse().map((badge, index) => {
+    const badges = props.badges.map((badge, index) => {
         return (
             <Badge 
                 key={index}
